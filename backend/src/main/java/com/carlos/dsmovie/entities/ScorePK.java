d6ada68ae07bb9aa749f3f8a3fe172c57a4b71fa
carlos4565mana <carloscal61@gmail.com>
2022-01-12 09:30:28 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/carlos/dsmovie/entities/ScorePK.java
@@ -0,0 +1,43 @@
+package com.carlos.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne  //configuracao de chave estrangeira
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ScorePK() {
+		
+	}
+	
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	
+
+}
